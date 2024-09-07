c842463924eb74d9a40d8021173fca350a19520f
lydiaworku <lworku@lion.lmu.edu>
2024-09-07 16:32:26 -0700

    python powers generator done

--- python/exercises.py
+++ python/exercises.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from collections.abc import Callable
 from typing import Optional
+from collections.abc import Callable
 
 
 def change(amount: int) -> dict[int, int]:
@@ -14,17 +14,31 @@ def change(amount: int) -> dict[int, int]:
     return counts
 
 
+
+# Write your first then lower case function here
+
 def first_then_lower_case(a: list[str], p: Callable[[str], bool]) -> Optional[str]:
     for a_string in a:
         if p(a_string):
             return a_string.lower()
-    return None 
+    return None
+
 
 
 # Write your powers generator here
+def powers_generator(base: int, limit: int):
+    power: int = 0
+    result: int = 0
+    while result < limit:
+        result = base**power
+        power += 1
+        if result <= limit: 
+            yield result
+
 
 
 # Write your say function here
+    
 
 
 # Write your line count function here
