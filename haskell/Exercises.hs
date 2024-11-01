3d3a8d3f43291817e216b3fdc57e1044d3df48d4
lydiaworku <lworku@lion.lmu.edu>
2024-11-01 15:07:12 -0700

    haskell comments

--- haskell/Exercises.hs
+++ haskell/Exercises.hs
@@ -33,44 +33,49 @@ firstThenApply xs pred f = fmap f (find pred xs)
 powers :: Integral num => num -> [num]
 powers base = map (base^) [0..]
 
-
+-- counts number of meaningful lines in a file
 meaningfulLineCount :: FilePath -> IO Int
 meaningfulLineCount filePath = do
     document <- readFile filePath
     let allWhitespace line = all isSpace line
         trimStart = dropWhile isSpace
-        isMeaningful line =
-            not (allWhitespace line) &&
-            not ("#" `isPrefixOf` (trimStart line))
+        isMeaningful line = 
+            not (allWhitespace line) && -- checks if line is all whitespace
+            not ("#" `isPrefixOf` (trimStart line)) -- checks if line starts with #
     return $ length $ filter isMeaningful (lines document)
 
-
+-- creates sphere and box shape objects
 data Shape
   = Sphere Double
   | Box Double Double Double
   deriving (Eq, Show)
 
+-- calculates volume of given shape
 volume :: Shape -> Double
 volume (Sphere r) = (4 / 3) * pi * r^3
 volume (Box l w h) = l * w * h
 
+-- calculates surface area of given shape
 surfaceArea :: Shape -> Double
 surfaceArea (Sphere r) = 4 * pi * r^2
 surfaceArea (Box l w h) = 2 * (l * w + l * h + w * h)
 
-
+-- creates BST that can hold Empty or Node objects
 data BST a
   = Empty
   | Node a (BST a) (BST a)
 
+-- calculates size of BST
 size :: BST a -> Int
 size Empty = 0
 size (Node _ left right) = 1 + size left + size right
 
+-- traverses the BST in order and returns the values
 inorder :: BST a -> [a]
 inorder Empty = []
 inorder (Node value left right) = inorder left ++ [value] ++ inorder right
 
+-- inserts a value into the BST
 insert :: Ord a => a -> BST a -> BST a
 insert value Empty = Node value Empty Empty
 insert value (Node nodeValue left right)
@@ -78,6 +83,7 @@ insert value (Node nodeValue left right)
   | value > nodeValue = Node nodeValue left (insert value right)
   | otherwise = Node nodeValue left right
 
+-- checks if the BST contains a given value
 contains :: Ord a => a -> BST a -> Bool
 contains _ Empty = False
 contains value (Node nodeValue left right)
@@ -85,7 +91,7 @@ contains value (Node nodeValue left right)
   | value < nodeValue = contains value left
   | otherwise = contains value right
 
-
+-- turns the BST into a string and returns it, taking care of superfluous parentheses
 instance (Show a) => Show (BST a) where
   show Empty = "()"
   show (Node value Empty Empty) = "(" ++ show value ++ ")"
